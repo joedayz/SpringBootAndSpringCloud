e01e8957a24bb9054a940f17891e69baf16a6c54
Jose Diaz <jose.diaz@joedayz.pe>
2018-07-07 10:32:35 -0500

    config service

---
+++ clase2/config-service/src/main/java/pe/joedayz/configservice/ConfigServiceApplication.java
@@ -0,0 +1,12 @@
+package pe.joedayz.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+}
