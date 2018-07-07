d70bf29c86c3dfe4318b57664bd090b21ec9cb30
Jose Diaz <jose.diaz@joedayz.pe>
2018-07-07 12:10:30 -0500

    application. yml

--- clase2/customer-service/src/main/java/pe/joedayz/customerservice/CustomerServiceApplication.java
+++ clase2/customer-service/src/main/java/pe/joedayz/customerservice/CustomerServiceApplication.java
@@ -1,14 +1,27 @@
 package pe.joedayz.customerservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "pe.joedayz")
 @EnableJpaRepositories("pe.joedayz.customerservice.dao.jpa")
+@EnableSwagger2
 @EnableDiscoveryClient
+@EnableFeignClients
 public class CustomerServiceApplication {
+
+    static Class applicationClass = CustomerServiceApplication.class;
+    private static final Logger log = LoggerFactory.getLogger(applicationClass);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
