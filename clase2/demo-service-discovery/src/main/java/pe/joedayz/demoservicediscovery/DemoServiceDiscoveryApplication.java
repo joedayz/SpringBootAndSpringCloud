3efb9c03c332941316ddf31448fb03ccbce8c155
Jose Diaz <jose.diaz@joedayz.pe>
2018-07-07 10:12:40 -0500

    discovery service

---
+++ clase2/demo-service-discovery/src/main/java/pe/joedayz/demoservicediscovery/DemoServiceDiscoveryApplication.java
@@ -0,0 +1,12 @@
+package pe.joedayz.demoservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoServiceDiscoveryApplication.class, args);
+	}
+}
